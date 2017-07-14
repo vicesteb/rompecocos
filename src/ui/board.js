c656c4908f38a22a584a78929cb404edfa825060
Vicente Esteban Bañuelos <vesteban@tacitknowledge.com>
2017-07-14 12:07:49 -0500

    Render squares dynamically

--- src/ui/board.js
+++ src/ui/board.js
@@ -2,49 +2,45 @@ import React from 'react';
 import Square from './square';
 
 class Board extends React.Component {
+
   constructor() {
     super();
+    
+    var i = 0;
 
     this.state = {
-      squares: Array.apply(null, {length: 16}).map(Number.call, Number)
+      squares:  Array(4).fill(Array(4)).map((row) => {
+        return row.fill(0).map((element) => {
+          return i++;
+        })  
+      })
     }
   }
 
   handleClick(i) {
-    alert('Clicked on ' + i);
+    alert('Clicked on square ' + i);
   }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
+    return <Square value={i} onClick={() => this.handleClick(i)}/>;
+  }
+
+  renderSquaresRow(squareRow) {
+    return (
+      <div className="board-row">
+        { squareRow.map(this.renderSquare, this) }
+      </div>
+    );
+  }
+
+  renderSquares() {
+    return this.state.squares.map(this.renderSquaresRow, this);
   }
 
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-          {this.renderSquare(3)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(8)}
-          {this.renderSquare(9)}
-          {this.renderSquare(10)}
-          {this.renderSquare(11)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(12)}
-          {this.renderSquare(13)}
-          {this.renderSquare(14)}
-          {this.renderSquare(15)}
-        </div>
+        { this.renderSquares() }
       </div>
     );
   }

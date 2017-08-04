ceeb4818728eadd9eada07c9add661e992203863
Vicente Esteban Bañuelos <vesteban@Vicentes-MacBook-Pro-2.local>
2017-08-04 08:37:05 -0500

    Lift state from board to game

--- src/ui/board.js
+++ src/ui/board.js
@@ -3,79 +3,8 @@ import Square from './square';
 
 class Board extends React.Component {
 
-  constructor() {
-    super();
-    
-    var i = 0;
-
-    this.state = {
-      squares:  Array(4).fill(Array(4)).map((row) => {
-        return row.fill(0).map((element) => {
-          return i++;
-        })
-      }),
-      emptySquare: {
-          rowIndex: 0,
-          squareIndex: 0
-      }      
-    }
-  }
-
-  clickedOnEmptySquare(rowIndex, squareIndex) {
-    return this.state.emptySquare.rowIndex === rowIndex && this.state.emptySquare.squareIndex === squareIndex;
-  }
-
-  isUpperSquareEmpty(rowIndex, squareIndex) {
-    return this.state.emptySquare.rowIndex === rowIndex -1 && this.state.emptySquare.squareIndex === squareIndex;
-  }
-
-  isLeftSquareEmpty(rowIndex, squareIndex) {
-    return this.state.emptySquare.rowIndex === rowIndex && this.state.emptySquare.squareIndex === squareIndex - 1;
-  }
-
-  isBottomSquareEmpty(rowIndex, squareIndex) {
-    return this.state.emptySquare.rowIndex === rowIndex + 1 && this.state.emptySquare.squareIndex === squareIndex;
-  }
-
-  isRightSquareEmpty(rowIndex, squareIndex) {
-    return this.state.emptySquare.rowIndex === rowIndex && this.state.emptySquare.squareIndex === squareIndex + 1;
-  }
-
-  clickedOnMovableSquare(rowIndex, squareIndex) {
-    return (this.isUpperSquareEmpty(rowIndex, squareIndex)
-              || this.isLeftSquareEmpty(rowIndex, squareIndex)
-                || this.isBottomSquareEmpty(rowIndex, squareIndex)
-                  || this.isRightSquareEmpty(rowIndex, squareIndex)) 
-  }
-
-  swapWithEmptySquare(rowIndex, squareIndex) {
-    this.setState(function(prevState, props) {
-        var newEmptySquare = {
-          rowIndex: rowIndex,
-          squareIndex: squareIndex
-        }
-        prevState.squares[prevState.emptySquare.rowIndex][prevState.emptySquare.squareIndex] = prevState.squares[rowIndex][squareIndex];
-        prevState.squares[rowIndex][squareIndex] = 0;
-
-        return {
-          squares : prevState.squares,
-          emptySquare: newEmptySquare
-        }
-    });
-  }
-
-  handleClick(rowIndex, squareIndex) {
-    //alert('Clicked on (' + rowIndex + ', ' + squareIndex + ') with value ' + this.state.squares[rowIndex][squareIndex]);
-
-    if(!this.clickedOnEmptySquare(rowIndex, squareIndex)
-        && this.clickedOnMovableSquare(rowIndex, squareIndex)) {
-
-          this.swapWithEmptySquare(rowIndex, squareIndex);
-    }
-  }
-
   renderSquare(rowIndex, squareIndex, squareValue) {
-    return <Square value={squareValue} onClick={() => this.handleClick(rowIndex, squareIndex)}/>;
+    return <Square value={squareValue} onClick={() => this.props.onClick(rowIndex, squareIndex)}/>;
   }
 
   renderSquaresRow(squareRow, rowIndex) {
@@ -87,7 +16,7 @@ class Board extends React.Component {
   }
 
   renderSquares() {
-    return this.state.squares.map(this.renderSquaresRow, this);
+    return this.props.squares.map(this.renderSquaresRow, this);
   }
 
   render() {
--- src/ui/game.js
+++ src/ui/game.js
@@ -2,11 +2,81 @@ import React from 'react';
 import Board from './board'
 
 class Game extends React.Component {
+  constructor() {
+    super();
+    
+    var i = 0;
+
+    this.state = {
+      squares:  Array(4).fill(Array(4)).map((row) => {
+          return row.fill(0).map((element) => {
+            return i++;
+          })
+      }),
+      emptySquare: {
+          rowIndex: 0,
+          squareIndex: 0
+      }      
+    }
+  }
+
+
+  clickedOnEmptySquare(rowIndex, squareIndex) {
+    return this.state.emptySquare.rowIndex === rowIndex && this.state.emptySquare.squareIndex === squareIndex;
+  }
+
+  isUpperSquareEmpty(rowIndex, squareIndex) {
+    return this.state.emptySquare.rowIndex === rowIndex -1 && this.state.emptySquare.squareIndex === squareIndex;
+  }
+
+  isLeftSquareEmpty(rowIndex, squareIndex) {
+    return this.state.emptySquare.rowIndex === rowIndex && this.state.emptySquare.squareIndex === squareIndex - 1;
+  }
+
+  isBottomSquareEmpty(rowIndex, squareIndex) {
+    return this.state.emptySquare.rowIndex === rowIndex + 1 && this.state.emptySquare.squareIndex === squareIndex;
+  }
+
+  isRightSquareEmpty(rowIndex, squareIndex) {
+    return this.state.emptySquare.rowIndex === rowIndex && this.state.emptySquare.squareIndex === squareIndex + 1;
+  }
+
+  clickedOnMovableSquare(rowIndex, squareIndex) {
+    return (this.isUpperSquareEmpty(rowIndex, squareIndex)
+              || this.isLeftSquareEmpty(rowIndex, squareIndex)
+                || this.isBottomSquareEmpty(rowIndex, squareIndex)
+                  || this.isRightSquareEmpty(rowIndex, squareIndex)) 
+  }
+
+  swapWithEmptySquare(rowIndex, squareIndex) {
+    this.setState(function(prevState, props) {
+        var newEmptySquare = {
+          rowIndex: rowIndex,
+          squareIndex: squareIndex
+        }
+        prevState.squares[prevState.emptySquare.rowIndex][prevState.emptySquare.squareIndex] = prevState.squares[rowIndex][squareIndex];
+        prevState.squares[rowIndex][squareIndex] = 0;
+
+        return {
+          squares : prevState.squares,
+          emptySquare: newEmptySquare
+        }
+    });
+  }
+
+  handleClick(rowIndex, squareIndex) {
+    // alert('Clicked on [' + rowIndex + ', ' + squareIndex + ']');
+    if(!this.clickedOnEmptySquare(rowIndex, squareIndex)
+        && this.clickedOnMovableSquare(rowIndex, squareIndex)) {
+          this.swapWithEmptySquare(rowIndex, squareIndex);
+    }
+  }
+
   render() {
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board onClick={(rowIndex, squareIndex) => this.handleClick(rowIndex, squareIndex)} squares={this.state.squares}/>
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
